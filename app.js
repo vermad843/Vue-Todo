c39a03c7d2b741ba0a00cfa4207e5909bc000306
vermad843 <vishnuverma7524@gmail.com>
2019-12-17 12:05:48 +0530

    form submitted on the console

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const app = new Vue({
     el : '#app',
     data : {
       title : 'Hello World'  
+    },
+    methods : {
+       addTodo() {
+           console.log('form Submitted')
+       }
     }
-})
+});
